0e38eacf7553bb0e077e772f2e1ae544b628f51d
Matt Werner <mrwerner392@gmail.com>
2019-04-03 16:12:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function shout(word) {
+function shout(string) {
   return word.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
 }
